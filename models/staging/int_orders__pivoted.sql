47d5a9365330c428577e8142fe24b58fdadf95fb
aishwarya.dubasi@aretove.com <aishwarya.dubasi@aretove.com>
2021-11-15 16:58:47 +0000

    .

--- models/staging/int_orders__pivoted.sql
+++ models/staging/int_orders__pivoted.sql
@@ -1,4 +1,4 @@
-{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'coupon'] -%}
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
 
 with payments as (
       SELECT * FROM {{ref('stg_payment')}}
@@ -8,21 +8,20 @@ with payments as (
 
         SELECT
             order_id,
-
-            
-
-            {%- for payment_method in payment_methods -%}
+            {% for payment_method in payment_methods -%}
 
              sum(case when payment_method = '{{payment_method}}' then amount else 0 end) as {{payment_method}}_amount
 
-
             {%- if not loop.last -%}
                  ,
-            {% endif -%}
+            {%- endif %}
 
-            {%- endfor %}
-        
+            {% endfor -%}
+       
+
             from payments
+
+            where status = 'success'
 
             group by 1
 
